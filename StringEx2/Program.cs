d47a424b910609bfecdeb6b23687ba1a1767b30c
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-08-15 15:15:46 +0700

    string function excercise

---
+++ StringEx2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "";
+
+            Console.WriteLine("Please enter your name and press enter: ");
+            myName = Console.ReadLine();
+            string myNameUpperCase = string.Format("Upper Case: {0}", myName.ToUpper());
+            string myNameLowerCase = string.Format("Lower Case: {0}", myName.ToLower());
+            string myNameTrimmed = string.Format("Trim the string: {0}", myName.Trim());
+            string myNameSubString = string.Format("Substring: {0}", myName.Substring(0, 5));
+
+           
+            Console.WriteLine(myNameUpperCase);
+            Console.WriteLine(myNameLowerCase);
+            Console.WriteLine(myNameTrimmed);
+            Console.WriteLine(myNameSubString);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
